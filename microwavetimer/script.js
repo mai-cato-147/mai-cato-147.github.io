dbd7a9953ffdd3eb46ffc49c7d7a0035a7447276
mai-cato-147 <mai.cato.147@gmail.com>
2025-06-08 00:31:35 +0900

    インデント調整

--- microwavetimer/script.js
+++ microwavetimer/script.js
@@ -1,18 +1,18 @@
 function calc() {
-  let origWValue = document.getElementById("origW").value
-  let origMinValue = document.getElementById("origMin").value
-  let origSecValue = document.getElementById("origSec").value
-  let targetWValue = document.getElementById("targetW").value
+	let origWValue = document.getElementById("origW").value
+	let origMinValue = document.getElementById("origMin").value
+	let origSecValue = document.getElementById("origSec").value
+	let targetWValue = document.getElementById("targetW").value
 
-  let result = origWValue * (parseFloat(origMinValue) + origSecValue / 60.0) / targetWValue
-  var min = Math.floor(result)
-  var sec = Math.round(60 * (result - min))
-  sec = Math.round(sec / 10) * 10
+	let result = origWValue * (parseFloat(origMinValue) + origSecValue / 60.0) / targetWValue
+	var min = Math.floor(result)
+	var sec = Math.round(60 * (result - min))
+	sec = Math.round(sec / 10) * 10
 
-  if (sec == 60) {
-    min++
-    sec = 0
-  }
+	if (sec == 60) {
+		min++
+		sec = 0
+	}
 
-  document.getElementById("result").textContent = targetWValue + "Wで" + min + "分" + sec + "秒です！"
+	document.getElementById("result").textContent = targetWValue + "Wで" + min + "分" + sec + "秒です！"
 }
